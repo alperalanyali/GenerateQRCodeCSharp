94d1d6b915e68bfbfa84d7df4d55de56c8a2489f
Alper Alanyali <30805005+alperalanyali@users.noreply.github.com>
2022-10-15 17:45:48 +0300

    Adding new folders

---
+++ GenerateQRCode/GenerateQRCode/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+//using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Ajax;
+using QRCoder;
+
+namespace GenerateQRCode.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+          var mvcName = typeof(Controller).Assembly.GetName();
+            var isMono = Type.GetType("Mono.Runtime") != null;
+
+            ViewData["Version"] = mvcName.Version.Major + "." + mvcName.Version.Minor;
+            ViewData["Runtime"] = isMono ? "Mono" : ".NET";
+
+            return View();
+        }
+        public ActionResult GenerateQR()
+        {
+            QRCodeGenerator qr = new QRCodeGenerator();
+            QRCodeData data = qr.CreateQrCode("Welcome to ALper", QRCodeGenerator.ECCLevel.Q);
+            QRCode code = new QRCode(data);
+            Bitmap bitmap = code.GetGraphic(15);
+            var bitmapBytes = ConvertBitmapToBytes(bitmap);
+            return File(bitmapBytes,"images/jpeg");
+
+        }
+
+        private byte[] ConvertBitmapToBytes(Bitmap bitmap)
+        {
+            using(MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                return ms.ToArray();
+            }
+        }
+      
+    }
+}
